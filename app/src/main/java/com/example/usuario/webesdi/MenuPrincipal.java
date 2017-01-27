3405bb494bd571cb84866a223057754d1d43a047
oscar <oscergar@gmail.com>
2017-01-27 12:27:00 +0100

    a ver si funciona

--- app/src/main/java/com/example/usuario/webesdi/MenuPrincipal.java
+++ app/src/main/java/com/example/usuario/webesdi/MenuPrincipal.java
@@ -1,17 +1,33 @@
 package com.example.usuario.webesdi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MenuPrincipal extends AppCompatActivity {
-
+    Button btnpaginaweb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
+        btnpaginaweb = (Button) findViewById(R.id.brnpaginaweb);
+
+        btnpaginaweb.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                lanzarActivity();
+
+                }
+        });
 
 
+    }
+    private void lanzarActivity(){
+        Intent intent = new Intent(this,PaginaWeb.class);
+        startActivity(intent);
     }
 
 
