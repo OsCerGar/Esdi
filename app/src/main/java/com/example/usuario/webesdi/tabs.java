32c0cd74ae46c4c61d51869932bfc9fc66a1353c
JR <topatacio@gmail.com>
2017-04-18 09:43:48 +0200

    2017-04-18 09:43

--- app/src/main/java/com/example/usuario/webesdi/tabs.java
+++ app/src/main/java/com/example/usuario/webesdi/tabs.java
@@ -33,32 +33,31 @@ import java.util.Map;
 public class tabs extends AppCompatActivity {
 
 
-    String consulta;
+   // String consulta;
     String correo;
     String rolMaster = "administrador"; //nombre del rol con control total
+    String miTab;
     TextView txtEmail;
     TextView txtChat;
     EditText inChat;
     String texto;
-    Spinner spDestino;
     Spinner spCorreo;
 
     TextView txtEmail2;
     TextView txtChat2;
     EditText inChat2;
-    String texto2;
-    Spinner spDestino2;
-    Spinner spCorreo2;
+
 
     TextView txtEmail3;
     TextView txtChat3;
     EditText inChat3;
-    String texto3;
-    Spinner spDestino3;
-    Spinner spCorreo3;
+
 
     Bundle b;
     ImageButton enviar;
+    ImageButton enviar2;
+    ImageButton enviar3;
+
     List<String> datos2;
     //private DatabaseReference dbMensajes;
     //private ValueEventListener eventListener;
@@ -81,117 +80,115 @@ public class tabs extends AppCompatActivity {
         inChat = (EditText) findViewById(R.id.inChat);
         txtEmail = (TextView) findViewById(R.id.txtEmail);
         txtChat = (TextView) findViewById(R.id.txtChat);
-        spDestino = (Spinner) findViewById(R.id.spDestino);
-        spCorreo = (Spinner) findViewById(R.id.spCorreo);
 
 
         inChat2 = (EditText) findViewById(R.id.inChat2);
         txtEmail2 = (TextView) findViewById(R.id.txtEmail2);
         txtChat2 = (TextView) findViewById(R.id.txtChat2);
-        spDestino2 = (Spinner) findViewById(R.id.spDestino2);
-        spCorreo2 = (Spinner) findViewById(R.id.spCorreo2);
 
 
         inChat3 = (EditText) findViewById(R.id.inChat3);
         txtEmail3 = (TextView) findViewById(R.id.txtEmail3);
         txtChat3 = (TextView) findViewById(R.id.txtChat3);
-        spDestino3 = (Spinner) findViewById(R.id.spDestino3);
-        spCorreo3 = (Spinner) findViewById(R.id.spCorreo3);
 
+        enviar = (ImageButton) findViewById(R.id.imageButton);
+        enviar2 = (ImageButton) findViewById(R.id.imageButton2);
+        enviar3 = (ImageButton) findViewById(R.id.imageButton3);
+        spCorreo = (Spinner) findViewById(R.id.spCorreo);
 
         correo = b.getString("email"); //inicialmente el correo es el del usuario logueado
 
         //bloque para dar valor a las pestañas
 
-        //  View includedLayout = findViewById(R.id.activity_mensajes);
-        //TextView insideTheIncludedLayout = (TextView)includedLayout.findViewById(R.id.txtChat);
 
         Resources res = getResources();
 
         TabHost tabs = (TabHost) findViewById(android.R.id.tabhost);
         tabs.setup();
 
-        TabHost.TabSpec spec = tabs.newTabSpec("mitab1");
+        TabHost.TabSpec spec = tabs.newTabSpec("Sugerencia");
         spec.setContent(R.id.tab1);
         spec.setIndicator("Sugerencias");
         tabs.addTab(spec);
 
-        spec = tabs.newTabSpec("mitab2");
+        spec = tabs.newTabSpec("Incidencia");
         spec.setContent(R.id.tab2);
         spec.setIndicator("Incidencias");
         tabs.addTab(spec);
 
-        spec = tabs.newTabSpec("mitab3");
+        spec = tabs.newTabSpec("Consulta");
         spec.setContent(R.id.tab3);
-        spec.setIndicator("consultas");
+        spec.setIndicator("Consultas");
         tabs.addTab(spec);
 
         tabs.setCurrentTab(0);
 
-        tabs.setOnTabChangedListener(new OnTabChangeListener() {
-            public void onTabChanged(String tabId) {
 
+        tabs.setOnTabChangedListener(new OnTabChangeListener() {
+            public void onTabChanged(final String tabId) {
+
+
+                enviar.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v){
+                        entraTexto(tabId);
+
+                    }
+                });
+                enviar2.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v){
+                        entraTexto(tabId);
+
+                    }
+                });
+                enviar3.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v){
+                        entraTexto(tabId);
+
+                    }
+                });
 
                 //switch con el resultado de la posicion seleccionadan,
                 switch (tabId) {
-                    case "mitab1":
-                        Log.d(TAGLOG, String.valueOf("=========================> " + tabId + " <==============="));
-
-
-                        //oculta el spinner de seleccion de usuario de consulta
-                        spCorreo.setVisibility(View.GONE);
+                    case "Sugerencia":
 
                         //llama a muestratexto y dependiendo del rol muestra los mensajes
                         //si es master oculta el boton enviar y muestra todos los mensajes
-                        //   prueba(tabId);
                         if (b.getString("rol").equals(rolMaster)) {
                             enviar.setVisibility(View.GONE);
-                            muestraMensajes("todos");
-                            //   Toast.makeText(getApplicationContext(), rolMaster, Toast.LENGTH_LONG).show();
+                            muestraMensajes("todos", tabId);
                         }
 
                         //si no es master permite enviar y muestra solo sus mensajes
-                        muestraMensajes(b.getString("email").toString());
+                        muestraMensajes(b.getString("email").toString(),tabId);
 
                         break;
-                    case "mitab2":
+                    case "Incidencia":
 
                         //// TODO: de momeneto esta hace lo mismo que sugerencia, hay que cambiarlo  para incidencias
-                        Log.d(TAGLOG, String.valueOf("=========================> " + tabId + " <==============="));
-                        Toast.makeText(getApplicationContext(), "cosa 2", Toast.LENGTH_LONG).show();
-                        //  prueba(tabId);
-                        txtChat.setText("tab doooos");
-
-
-                        //oculta el spinner de seleccion de usuario de consulta
-                        spCorreo.setVisibility(View.GONE);
 
                         //llama a muestratexto y dependiendo del rol muestra los mensajes
                         //si es master oculta el boton enviar y muestra todos los mensajes
 
                         if (b.getString("rol").equals(rolMaster)) {
-                            enviar.setVisibility(View.GONE);
-                            muestraMensajes("todos");
+                            enviar2.setVisibility(View.GONE);
+                            muestraMensajes("todos",tabId);
                         }
                         //si no es master puede enviar y muestra solo sus mensajes
-                        muestraMensajes(b.getString("email").toString());
+                        muestraMensajes(b.getString("email").toString(),tabId);
 
                         break;
-                    case "mitab3":
-                        prueba(tabId);
+                    case "Consulta":
                         //si es master llama a creaspinnercorreo para seleccionar la conversacion
-                        Log.d(TAGLOG, String.valueOf("=========================> " + tabId + " <==============="));
-                        Toast.makeText(getApplicationContext(), "cosa 3", Toast.LENGTH_LONG).show();
 
                         if ((b.getString("rol")).equals(rolMaster)) {
-                            creaSpinnerCorreo();
+                            creaSpinnerCorreo(miTab);
                             spCorreo.setVisibility(View.VISIBLE);
                             //si no es master oculta el spiner de seleccion de conversacion, permite enviar
                             //mensajes de consulta y muestra los mensajes propios
                         } else {
                             spCorreo.setVisibility(View.GONE);
-                            enviar.setVisibility(View.VISIBLE);
-                            muestraMensajes(b.getString("email").toString());
+                            enviar3.setVisibility(View.VISIBLE);
+                            muestraMensajes(b.getString("email").toString(),tabId);
                         }
 
                         break;
@@ -200,17 +197,15 @@ public class tabs extends AppCompatActivity {
                         break;
                 }
 
+
             }
         });
         //fin de pestañas
 
     }
 
-    public void prueba(String texto) {
-        txtChat.setText("desdeprueba " + texto);
-    }
 
-    public void creaSpinnerCorreo() {
+    public void creaSpinnerCorreo(final String miTab) {
         //definición del spinner de conversacion
         //crea una lista dinamica y se carga con los elementos recibidos de la BD
 
@@ -218,7 +213,7 @@ public class tabs extends AppCompatActivity {
         datos2.add("todos");
 
         //instancia la base de datos de firebase
-        DatabaseReference dbMensajes = FirebaseDatabase.getInstance().getReference().child(consulta);
+        DatabaseReference dbMensajes = FirebaseDatabase.getInstance().getReference().child(miTab);
         Query dbQuery = dbMensajes.orderByKey();
 
         ValueEventListener eventListener = new ValueEventListener() {
@@ -255,23 +250,23 @@ public class tabs extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, android.view.View v, int position, long id) {
 
                 correo = parent.getItemAtPosition(position).toString();
-                txtEmail.setText(consulta + " de " + correo);
+                txtEmail3.setText(miTab + " de " + correo);
 
                 switch (spCorreo.getSelectedItemPosition()) {
                     case 0:
                         //si se selecciona todos, oculta el boton enviar
-                        enviar.setVisibility(View.GONE);
+                        enviar3.setVisibility(View.GONE);
                         //   Toast.makeText(getApplicationContext(), "todos seleccionados "+spCorreo.getSelectedItemPosition(), Toast.LENGTH_LONG).show();
                         break;
                     default:
                         //por defecto muestra el boton enviar
-                        enviar.setVisibility(View.VISIBLE);
+                        enviar3.setVisibility(View.VISIBLE);
                         //   Toast.makeText(getApplicationContext(), "otros seleccionados "+spCorreo.getSelectedItemPosition(), Toast.LENGTH_LONG).show();
                         break;
                 }
 
-                //llama a muestra mensaje con el correo a mostrar o con "todos" si no se ha seleccionado ninguno
-                muestraMensajes(correo);
+                //llama a muestra mensaje con el correo seleccionado a mostrar o con "todos" si no se ha seleccionado ninguno
+                muestraMensajes(correo, miTab);
             }
 
             //metodo del spinner cuando no se selecciona nada
@@ -282,81 +277,103 @@ public class tabs extends AppCompatActivity {
         });
     }
 
-    public void setTextviews(String miTab){
-        switch (miTab){
-            case "mitab01":
-                break;
-            case "mitab02":
-                break;
-            case "mitab03":
-                break;
-            default:
-                break;
-        }
-    }
 
-
-    public void muestraMensajes(final String correo) {
+    public void muestraMensajes(final String correo, final String miTab) {
 
         //instancia la base de datos de firebase
-        DatabaseReference dbMensajes = FirebaseDatabase.getInstance().getReference().child(consulta);
+        DatabaseReference dbMensajes = FirebaseDatabase.getInstance().getReference().child(miTab);
 
         //hace una consulta para mostrar los mensajes ordenados por fecha
         Query dbQuery = dbMensajes.orderByKey();
-        txtChat.setText("");
 
         ValueEventListener eventListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot nodoUsuario) {
-                texto = "";
+
+                  texto = "";
                 for (DataSnapshot childDataSnapshot : nodoUsuario.getChildren()) {
+                    Log.d(TAGLOG, "----------------------- pasndo---------------");
 
                     //si el usuario logueado es master y selecciona "todos" en el spinner, muestra todos los mensajes, si no muestra solo
                     //los que coincide su correo (que le pasamos al llamar al procedimiento) con el del correo del mensaje de la DB
                     if (correo.equals("todos")) {
-                        texto = (texto + childDataSnapshot.child("Nombre").getValue() + ": " + childDataSnapshot.child("Mensaje").getValue() + "\n");
+                        texto = (texto + childDataSnapshot.child("Nombre").getValue() + ": "
+                                + childDataSnapshot.child("Mensaje").getValue() + "\n");
 
                     } else if ((childDataSnapshot.child("Correo").getValue()).equals(correo)) {
-                        texto = (texto + childDataSnapshot.child("Nombre").getValue() + ": " + childDataSnapshot.child("Mensaje").getValue() + "\n");
+                        texto = (texto + childDataSnapshot.child("Nombre").getValue() + ": "
+                                + childDataSnapshot.child("Mensaje").getValue() + "\n");
                     }
 
-                    //carga el nuevo texto al textview
-                    txtChat.setText(texto);
+                    switch (miTab){
+                        case "Sugerencia":
+                            txtChat.setText(texto);
+                            txtChat.setMovementMethod(new ScrollingMovementMethod());
+                            break;
+                        case "Incidencia":
+                            txtChat2.setText(texto);
+                            txtChat2.setMovementMethod(new ScrollingMovementMethod());
+                            break;
+                        case "Consulta":
+                            txtChat3.setText(texto);
+                            txtChat3.setMovementMethod(new ScrollingMovementMethod());
+                            break;
+                        default:
+                            break;
+                    }
                 }
-
-                //permite el scroll de la ventana del textview
-                txtChat.setMovementMethod(new ScrollingMovementMethod());
-
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                Log.e(TAGLOG, "Error!", databaseError.toException());
+                Log.d(TAGLOG, "Error!", databaseError.toException());
             }
         };
 
 
         dbQuery.addValueEventListener(eventListener);
 
+
     }
 
 
     //metodo que se ejecuta al clickar enviar
-    public void entraTexto(View v) {
+    public void entraTexto(String miTab) {
         //instancia la base de datos de firebase
-        DatabaseReference dbEnviar = FirebaseDatabase.getInstance().getReference().child(consulta);
+        DatabaseReference dbEnviar = FirebaseDatabase.getInstance().getReference().child(miTab);
         //crea una lista con datos del usuario y el mensaje introducido
         Map<String, String> envio = new HashMap<>();
         envio.put("Correo", correo);
         envio.put("Nombre", b.getString("nombre"));
-        envio.put("Mensaje", inChat.getText().toString());
+
+        //selecciona una de las 3 cajas de entrada (una por pestaña) segun la pestaña y pone el valor
+        //en el array
+        switch (miTab){
+            case "Sugerencia":
+                envio.put("Mensaje", inChat.getText().toString());
+                //vacia la caja de entrada de texto
+                inChat.setText("");
+                break;
+            case "Incidencia":
+                envio.put("Mensaje", inChat2.getText().toString());
+                //vacia la caja de entrada de texto
+                inChat2.setText("");
+                break;
+            case "Consulta":
+                envio.put("Mensaje", inChat3.getText().toString());
+                //vacia la caja de entrada de texto
+                inChat3.setText("");
+                break;
+            default:
+                break;
+        }
+
 
         //sube el texto entrado a firebase, al nodo del usuario
         //  dbMensajes.child(txtEmail.getText().toString()).push().setValue(inChat.getText().toString());
         dbEnviar.push().setValue(envio);
 
-        //vacia la caja de entrada de texto
-        inChat.setText("");
+
 
     }
 
