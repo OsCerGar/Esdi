48322f9e0ddf5426892de8a5e6432ddca678ed93
silvan <silvan.zamora@gmail.com>
2017-05-10 17:46:20 +0200

    Añadida base para actividad de ayuda

--- app/src/main/java/com/example/usuario/webesdi/Ayuda.java
+++ app/src/main/java/com/example/usuario/webesdi/Ayuda.java
@@ -1,7 +1,11 @@
 package com.example.usuario.webesdi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Ayuda extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class Ayuda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda);
+
+        String texto = "Esta es la pagina de ayuda. De momento no hay nada pero, por si te sirve de ayuda, estamos trabajando en ello. Si no te sirve... Pues no se... " +
+                "Llama al 112, esta gente siempre esta dispuesta a ayudar, es más, les pagan por ello. " +
+                "Tambien puedes ir a la página de contacto y enviranos un mail o llamarnos." +
+                " Mira, de hecho, te voy a dejar un boton aqui debajo que te lleva alli, asi que, si quieres, púlsalo." +
+                "\n \n \n O no lo hagas, no soy tu jefe...";
+
+        ((TextView) findViewById(R.id.txtAyuda)).setText(texto);
+        ((Button) findViewById(R.id.btnAyuda)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Bundle b = getIntent().getExtras();
+                Intent intent = new Intent(getBaseContext(),Contacto.class);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
     }
 }
