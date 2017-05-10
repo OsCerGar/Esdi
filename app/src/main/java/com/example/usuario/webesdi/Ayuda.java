60e1108350480c0e3f3bcba271b23c6f067cf0b0
silvan <silvan.zamora@gmail.com>
2017-05-10 17:49:36 +0200

    Añadida base para actividad de ayuda

--- app/src/main/java/com/example/usuario/webesdi/Ayuda.java
+++ app/src/main/java/com/example/usuario/webesdi/Ayuda.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Ayuda extends AppCompatActivity {
+public class Ayuda extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
