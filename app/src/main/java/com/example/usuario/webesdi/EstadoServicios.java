8b850d5b0a0ea3f831a50d02ca6aeb3d25814058
EugenioGitoerrezC <eugeniogutierrez.c@gmail.com>
2017-06-15 12:03:22 +0200

    Serveis actualitzats

--- app/src/main/java/com/example/usuario/webesdi/EstadoServicios.java
+++ app/src/main/java/com/example/usuario/webesdi/EstadoServicios.java
@@ -69,7 +69,7 @@ public class EstadoServicios extends AppCompatActivity {
         @Override
 
         protected Boolean doInBackground(Void... params) {
-            WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
             final List<ScanResult> results = wifi.getScanResults();
             if (results != null) {
                 for (int i = 0; i < results.size(); i++) {
